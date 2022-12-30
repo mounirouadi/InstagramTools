b4f601548667c612f2ad313b8c36c08d0aac1c49
Mounir Ouadi <mounir.ouadi@gmail.com>
2022-12-30 16:51:08 +0100

    Passing usernames list by command line argument.

--- unfollow.py
+++ unfollow.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
-
+import sys
 from time import *
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
         
 def check_exists_by_xpath(xpath):
     try:
-        driver.find_element_by_xpath(xpath)
+        driver.find_element(By.XPATH,xpath)
     except NoSuchElementException:
         return False
     return True
@@ -18,52 +19,60 @@ def remove_html_tags(text):
     return re.sub(clean, ' ', text)
 
 
-url = "https://facebook.com"
-options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=C:\\Users\\dell\\AppData\\Local\\Google\\Chrome\\User Data\\test")
 
-driver = webdriver.Chrome(executable_path=r'C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe', chrome_options=options)
 
-my_file = open("usernames.txt", "r")
-content_list = my_file.readlines()
-final = []
-for i in content_list:
-    final.append(i.rstrip('\n'))
+if(len(sys.argv)>1):
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir=C:\\Users\\dell\\AppData\\Local\\Google\\Chrome\\User Data\\test")
+    options.add_argument('--disable-logging') 
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-final2 = final
+    driver = webdriver.Chrome(executable_path=r'C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe', chrome_options=options)
 
-print("there are : " + str(len(final)) + " accounts.")
-cnt = 0
-for i in final:
-    ddd = True
-    driver.get("https://www.instagram.com/" + i)
-    sleep(2)
-    if(check_exists_by_xpath("//button[@class='_acan _aiit _acap _aijb _acat _aj1-']")):
-        button2 = driver.find_element_by_xpath("//button[@class='_acan _aiit _acap _aijb _acat _aj1-']")
-        button2.click()
-        sleep(.2)
-        button3 = driver.find_element_by_xpath("//button[@class='_a9-- _a9-_']")
-        button3.click()
-        print(i + " unfollowed " + str(cnt))
-        cnt+=1
-        sleep(1)
-        driver.refresh()
+    file = sys.argv[1]
+
+    my_file = open(file, "r")
+    content_list = my_file.readlines()
+    final = []
+    for i in content_list:
+        final.append(i.rstrip('\n'))
+
+    final2 = final
+
+    print("there are : " + str(len(final)) + " accounts.")
+    cnt = 0
+    for i in final:
+        ddd = True
+        driver.get("https://www.instagram.com/" + i)
         sleep(2)
-        if(not check_exists_by_xpath("//button[@class='_acan _aiit _acap _aijb _acat _aj1-']")):
-            final2.remove(i)
+        if(check_exists_by_xpath("//button[@class='_acan _aiit _acap _aijb _acat _aj1-']")):
+            button2 = driver.find_element(By.XPATH,"//button[@class='_acan _aiit _acap _aijb _acat _aj1-']")
+            button2.click()
+            sleep(.2)
+            button3 = driver.find_element(By.XPATH,"//button[@class='_a9-- _a9-_']")
+            button3.click()
+            print(i + " unfollowed " + str(cnt))
+            cnt+=1
+            sleep(1)
+            driver.refresh()
+            sleep(2)
+            if(not check_exists_by_xpath("//button[@class='_acan _aiit _acap _aijb _acat _aj1-']")):
+                final2.remove(i)
+            else:
+                print("You've reached your limit.")
+                break
         else:
-            print("You've reached your limit.")
-            break
-    else:
-        print(i + " is already unfollowed.")
-        final2.remove(i)
+            print(i + " is already unfollowed.")
+            final2.remove(i)
 
 
 
-driver.close()
-print("there are : " + str(len(final2)) + " accounts left.")
-textfile = open("usernames.txt", "w")
-for element in final2:
-    textfile.write(element + "\n")
-textfile.close()
-#driver.delete_all_cookies()
+    print("there are : " + str(len(final2)) + " accounts left.")
+    textfile = open(file, "w")
+    for element in final2:
+        textfile.write(element + "\n")
+    textfile.close()
+    driver.quit()
+
+else:
+    print("Enter the username textfile path!")
